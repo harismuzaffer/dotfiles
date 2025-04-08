8056b96c25931c83122f492c5ce1494ae0ddc092
haris <harismuzaffer@gmail.com>
2025-04-08 15:39:55 +0530

    use colormenu for comp menu

--- config/.config/nvim/lua/config/blink.lua
+++ config/.config/nvim/lua/config/blink.lua
@@ -18,42 +18,18 @@ M.opts = {
     menu = {
       draw = {
         columns = {
-          { "label", "label_description", gap = 1 },
-          { "kind_icon", "kind" },
+          { "label", gap = 1 },
+          { "kind_icon" }, 
         },
         components = {
-          kind_icon = {
+          label = {
             text = function(ctx)
-              local lspkind = require("lspkind")
-              local icon = ctx.kind_icon
-              if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                  if dev_icon then
-                    icon = dev_icon
-                      end
-                  else
-                    icon = require("lspkind").symbolic(ctx.kind, {
-                        mode = "symbol",
-                        })
-            end
-
-              return icon .. ctx.icon_gap
-              end,
-
-              -- Optionally, use the highlight groups from nvim-web-devicons
-                -- You can also add the same function for `kind.highlight` if you want to
-                -- keep the highlight groups in sync with the icons.
-                highlight = function(ctx)
-                local hl = ctx.kind_hl
-                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                  local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                    if dev_icon then
-                      hl = dev_hl
-                        end
-                        end
-                        return hl
-                        end,
-          }
+              return require("colorful-menu").blink_components_text(ctx)
+            end,
+            highlight = function(ctx)
+              return require("colorful-menu").blink_components_highlight(ctx)
+            end,
+          },
         }
       }
     },
